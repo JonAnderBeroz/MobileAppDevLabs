bfedfdaf977b0d7be826db85057e03437e9cd684
Jon Ander <jonberoz2000@gmail.com>
2021-10-09 18:17:43 +0100

    Navigation to settings

--- weatherForecastApp/app/src/main/java/rgu/ac/wheaterforecast/MainActivity.java
+++ weatherForecastApp/app/src/main/java/rgu/ac/wheaterforecast/MainActivity.java
@@ -1,8 +1,13 @@
 package rgu.ac.wheaterforecast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.Navigation;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.more_options:
+                Navigation.findNavController( this, R.id.fragmentContainerView) .navigate(R.id.settingsFragment);
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
